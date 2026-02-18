3882005ad09e6ab1d70dca38eae963021495ab66
raut-abhishek <iamabhishekraut01@gmail.com>
2026-02-18 22:21:42 +0530

    model creation and training

--- ML-basics/models/classification.py
+++ ML-basics/models/classification.py
@@ -16,3 +16,10 @@ y = iris.target
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
+
+
+# Create model
+model = LogisticRegression(max_iter=200)
+
+# Train model
+model.fit(X_train, y_train)
